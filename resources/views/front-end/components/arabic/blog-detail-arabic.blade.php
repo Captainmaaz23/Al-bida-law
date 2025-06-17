dc6f3dc2a15af69a9f296ad2504554b3ba9d4bc7
Maaz Baloch <you@example.com>
2025-06-17 17:44:59 +0500

    Maaz

--- resources/views/front-end/components/arabic/blog-detail-arabic.blade.php
+++ resources/views/front-end/components/arabic/blog-detail-arabic.blade.php
@@ -1,101 +1,121 @@
  <!-- Page banner Area -->
  <div class="page-banner bg-1">
-    <div class="d-table">
-        <div class="d-table-cell">
-            <div class="container">
-                <div class="page-content">
-                    <h2>تفاصيل المدونة</h2>
-                    <ul>
-                        <li><a href="">بيت <i class="las la-angle-right"></i></a></li>
-                        <li>تفاصيل المدونة</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End Page banner Area -->
+     <div class="d-table">
+         <div class="d-table-cell">
+             <div class="container">
+                 <div class="page-content">
+                     <h2>تفاصيل المدونة</h2>
+                     <ul>
+                         <li><a href="">بيت <i class="las la-angle-right"></i></a></li>
+                         <li>تفاصيل المدونة</li>
+                     </ul>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- End Page banner Area -->
 
-<!-- Start Services Details Area -->
-<div class="services-details-area ptb-100">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-8 col-md-7 col-sm-12 text-end" dir="rtl">
-                <div class="services-details">
-                    <div class="img">
-                        <img src="{{ url('public/uploads/blogs/'.$blogs->image) }}" alt="Image">
-                    </div>
-                    <div class="services-details-content">
-                        <h3>{{$blogs->arabic_title}}</h3>
-                        <ul class="blog-list">
-                            <li>
-                                <i class="las la-calendar"></i>
-                                {{ $blogs->created_at->format('h:i A') }}
-                            </li>
-                            <li>
-                                <i class="las la-user-tie"></i>
-                                <a href="javascript:void(0)">{{ optional($blogs->user)->name }}</a>
-                            </li>
-                        </ul>
-                        <div style="text-align: justify; line-height: 2;">
-                            {!! $blogs->arabic_description !!}
-                        </div>
-                        
-                        
-                        {{-- <blockquote class="blockquote">
+ <!-- Start Services Details Area -->
+ <div class="services-details-area ptb-100">
+     <div class="container">
+         <div class="row justify-content-center">
+             <div class="col-lg-8 col-md-7 col-sm-12 text-end" dir="rtl">
+                 <div class="services-details">
+                     <div class="img">
+                         <img src="{{ url('public/uploads/blogs/' . $blogs->image) }}" alt="Image">
+                     </div>
+                     <div class="services-details-content">
+                         <h3>{{ $blogs->arabic_title }}</h3>
+                         <ul class="blog-list">
+                             <li>
+                                 <i class="las la-calendar"></i>
+                                 {{ $blogs->created_at->format('h:i A') }}
+                             </li>
+                             <li>
+                                 <i class="las la-user-tie"></i>
+                                 <a href="javascript:void(0)">{{ optional($blogs->user)->name }}</a>
+                             </li>
+                         </ul>
+                         <div style="text-align: justify; line-height: 2;">
+                             {!! $blogs->arabic_description !!}
+                         </div>
+
+
+                         {{-- <blockquote class="blockquote">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                             <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
                         </blockquote> --}}
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-5 col-sm-12 text-end" dir="rtl">
-                <div class="side-bar">
-                    <div class="side-bar-box recent-post">
-                        <h3 class="title">المشاركة الأخيرة</h3>
-                        @foreach ($recent_blogs as $recent)
-                            <div class="single-recent-post">
-                                <div class="recent-post-img">
-                                    <a href="{{ route('front.single_arabic_blog',$recent->id) }}"><img src="{{ url('public/uploads/blogs/',$recent->image) }}" alt="Image"></a>
-                                </div>
-                                <div class="recent-post-content">
-                                    <div class="row mb-4 justify-content-between text-nowrap" style="direction: rtl;">
-                                        <div class="col-auto ms-2">
-                                            <i class="las la-calendar"></i>
-                                            {{ arabicDiffForHumans($recent->created_at) }}
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="las la-user-alt"></i>
-                                            {!!optional($recent->user)->name ?? 'زائر' !!}
-                                        </div>
-                                    </div>
-                                    <h3><a href="blog-details.html">{{ $recent->arabic_title }}</a></h3>
-                                </div>
-                            </div>
-                        @endforeach                        
-                    </div>
-                    
-                    <div class="side-bar-box categories-box">
-                        <h3 class="title">فئات</h3>
-                        <ul>
-                            @foreach ($services as $service)
-                            <li><a href="{{ route('front.single_arabic_blog',$recent->id) }}"><i class="las la-angle-double-right"></i></i> {{$service->title_arabic}} </a></li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    
-                    <div class="side-bar-box tags-box">
-                        <h3 class="title">العلامات</h3>
-                        <ul>
-                            @foreach ($tags as $tag)
-                            <li><a href="javascript:void(0)">{{$tag->arabic_tag}}</a></li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End Services Details Area Area -->
+                     </div>
+
+                 </div>
+             </div>
+             <div class="col-lg-4 col-md-5 col-sm-12 text-end" dir="rtl">
+                 <div class="side-bar">
+                     <div class="side-bar-box recent-post">
+                         <h3 class="title">المشاركة الأخيرة</h3>
+                         @foreach ($recent_blogs as $recent)
+                             <div class="single-recent-post" dir="rtl">
+                                 <div class="d-flex justify-content-end">
+                                     <!-- Image on the right (visual left in RTL) -->
+                                     <div class="recent-post-img" style="flex-shrink: 0;">
+                                         <a href="{{ route('front.single_arabic_blog', $recent->id) }}">
+                                             <img src="{{ url('public/uploads/blogs/' . $recent->image) }}"
+                                                 alt="Image" class="w-100"
+                                                 style="max-width: 150px; border-radius: 5px;">
+                                         </a>
+                                     </div>
+
+                                     <!-- Text content on the left (visual right in RTL) -->
+                                     <div class="recent-post-content p-3" style="text-align: right;">
+                                         <h3 class="mb-3">
+                                             <a href="{{ route('front.single_arabic_blog', $recent->id) }}"
+                                                 class="text-decoration-none">
+                                                 {{ $recent->arabic_title }}
+                                             </a>
+                                         </h3>
+
+                                         <div class="d-flex justify-content-between text-nowrap">
+                                             <!-- Date first (right side in RTL) -->
+                                             <div class="d-flex align-items-center ms-3">
+                                                 <i class="las la-calendar ms-2"></i>
+                                                 <span>{{ arabicDiffForHumans($recent->created_at) }}</span>
+                                             </div>
+
+                                             <!-- Author second (left side in RTL) -->
+                                             <div class="d-flex align-items-center">
+                                                 <i class="las la-user-alt ms-2"></i>
+                                                 <span>{!! optional($recent->user)->name ?? 'زائر' !!}</span>
+                                             </div>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+                         @endforeach
+                     </div>
+
+                     <div class="side-bar-box categories-box">
+                         <h3 class="title">فئات</h3>
+                         <ul>
+                             @foreach ($services as $service)
+                                 <li><a href="{{ route('front.single_arabic_blog', $recent->id) }}"><i
+                                             class="las la-angle-double-left"></i></i> {{ $service->title_arabic }}
+                                     </a></li>
+                             @endforeach
+                         </ul>
+                     </div>
+
+                     <div class="side-bar-box tags-box">
+                         <h3 class="title">العلامات</h3>
+                         <ul>
+                             @foreach ($tags as $tag)
+                                 <li><a href="javascript:void(0)">{{ $tag->arabic_tag }}</a></li>
+                             @endforeach
+                         </ul>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- End Services Details Area Area -->
--- resources/views/front-end/components/arabic/single_services.blade.php
+++ resources/views/front-end/components/arabic/single_services.blade.php
@@ -22,41 +22,45 @@
 <!-- Start Services Details Area -->
 <div class="services-details-area ptb-100">
     <div class="container">
-        <div class="row justify-content-center" >
+        <div class="row justify-content-center">
             <div class="col-lg-8 col-md-7 col-sm-12 text-end" dir="rtl">
                 <div class="services-details">
                     <div class="img">
-                        <img src="{{ url('public/uploads/services',$single_service->image) }}" alt="Image">
+                        <img src="{{ url('public/uploads/services', $single_service->image) }}" alt="Image">
                     </div>
                     <div class="services-details-content">
                         <h3>{{ $single_service->title_arabic }}</h3>
-                        
+
                         <p>{!! $single_service->arabic_description !!}</b></p>
-                      
+
                     </div>
                     <div class="article-footer">
 
-                        
+
 
                         <div class="article-share">
                             <ul class="social">
                                 <li><span>يشارك :</span></li>
-                                <li><a href="https://www.facebook.com/login/"><i class="lab la-facebook-f pt-2"></i></a></li>
-                                <li><a href="https://twitter.com/i/flow/login" class="twitter pt-2" target="_blank"><i class="lab la-twitter"></i></a></li>
-                                <li><a href="https://www.instagram.com/" class="linkedin pt-2" target="_blank"><i class="lab la-linkedin-in"></i></a></li>
-                                <li><a href="https://www.google.co.uk/" class="instagram pt-2" target="_blank"><i class="lab la-instagram"></i></a></li>
-                                
+                                <li><a href="https://www.facebook.com/login/"><i class="lab la-facebook-f pt-2"></i></a>
+                                </li>
+                                <li><a href="https://twitter.com/i/flow/login" class="twitter pt-2" target="_blank"><i
+                                            class="lab la-twitter"></i></a></li>
+                                <li><a href="https://www.instagram.com/" class="linkedin pt-2" target="_blank"><i
+                                            class="lab la-linkedin-in"></i></a></li>
+                                <li><a href="https://www.google.co.uk/" class="instagram pt-2" target="_blank"><i
+                                            class="lab la-instagram"></i></a></li>
+
                             </ul>
                         </div>
 
                         <div class="article-tags">
                             <span><i class="las la-tags text-end"></i></span>
                             @foreach ($blog_tag as $tag)
-                            <a href="" class="px-2">{{$tag->arabic_tag}},</a>
+                                <a href="" class="px-2">{{ $tag->arabic_tag }},</a>
                             @endforeach
                         </div>
                     </div>
-                   
+
                 </div>
             </div>
             <div class="col-lg-4 col-md-5 col-sm-12 text-end">
@@ -64,42 +68,65 @@
                     <div class="side-bar-box recent-post" x>
                         <h3 class="title">قصص النجاح</h3>
                         @foreach ($recent_services as $recent)
-                        <div class="single-recent-post">
-                            <div class="recent-post-img">
-                                <a href="{{ route('front.single_arabic_service', $recent->id) }}"><img src=" {{ url('public/uploads/services/'.$recent->image) }} " alt="Image"></a>
-                            </div>
-                            <div class="recent-post-content" class="text-end">
-                                <div class="row mb-4 justify-content-between text-nowrap">
-                                    <div class="col-auto ms-2">
-                                        <i class="las la-calendar"></i>
-                                        {{ arabicDiffForHumans($recent->created_at) }}
+                            <div class="single-recent-post" dir="rtl">
+                                <div class="d-flex justify-end">
+                                    <!-- Image on the right -->
+                                    <div class="recent-post-img" style="flex-shrink: 0;">
+                                        <a href="{{ route('front.single_arabic_service', $recent->id) }}">
+                                            <img src="{{ url('public/uploads/services/' . $recent->image) }}"
+                                                alt="Image" class="w-100"
+                                                style="max-width: 150px; border-radius: 5px;">
+                                        </a>
                                     </div>
-                                    <div class="col-auto">
-                                        <i class="las la-user-alt"></i>
-                                        {!!optional($recent->user)->name ?? 'زائر' !!}
+
+                                    <!-- Text content on the left -->
+                                    <div class="recent-post-content p-3" style="text-align: left;">
+                                        <h3 class="text-end mb-3">
+                                            <a href="{{ route('front.single_arabic_service', $recent->id) }}"
+                                                class="text-decoration-none">
+                                                {!! Str::limit($recent->arabic_description, 60) !!}
+                                            </a>
+                                        </h3>
+
+                                        <div class="d-flex justify-content-between text-nowrap flex-row-reverse">
+                                            <!-- Date (on the right in RTL) -->
+                                            <div class="d-flex align-items-center ms-3">
+                                                <i class="las la-calendar ml-4"></i>
+                                                <span>{{ arabicDiffForHumans($recent->created_at) }}</span>
+                                            </div>
+
+                                            <!-- Author (on the left in RTL) -->
+                                            <div class="d-flex align-items-center">
+                                                <i class="las la-user-alt ml-4"></i>
+                                                <span>{!! optional($recent->user)->name ?? 'زائر' !!}</span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <h3><a href="{{ route('front.single_arabic_service', $recent->id) }}">{!! Str::limit($recent->arabic_description,60) !!}</a></h3>
                             </div>
-                        </div>
                         @endforeach
                     </div>
-                    
+
                     <div class="side-bar-box categories-box">
                         <h3 class="title">خدمات</h3>
                         <ul>
                             {{-- <li><a href=""><i class="las la-angle-double-right"></i></i> News</a></li> --}}
                             @foreach ($services_name as $service)
-                            <li><a href=""><i class="las la-angle-double-right"></i></i>{{$service->title_arabic}}</a></li>   
+                                <li>
+                                    <a href="" class="d-flex justify-content-end align-items-center">
+                                        <span>{{ $service->title_arabic }}</span>&nbsp;&nbsp;&nbsp;
+                                        <i class="las la-angle-double-left"></i>
+                                    </a>
+                                </li>
                             @endforeach
                         </ul>
                     </div>
-                    
+
                     <div class="side-bar-box tags-box">
                         <h3 class="title">العلامات</h3>
                         <ul>
                             @foreach ($blog_tag as $tag)
-                            <li><a href="">{{$tag->arabic_tag}}</a></li>
+                                <li><a href="">{{ $tag->arabic_tag }}</a></li>
                             @endforeach
                         </ul>
                     </div>
